d8f838744a0ea9c7a5affd38ddfd5fdf8b332352
Iskan9 <i_kh_1997@mail.ru>
2020-02-26 00:11:30 +0300

    Добавил записи из базы данных на домашнюю стр

--- accounts/views.py
+++ accounts/views.py
@@ -2,12 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+from .models import * # импортируем наши модели
+
+
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all() # запросить из базы все заказы
+    customers = Customer.objects.all()  # запросить из базы всех клиентов
+
+    total_customers = customers.count()  # количестов всех клиентов
+    total_orders = orders.count()  # количестов всех заказов
+    delivered = orders.filter(status='Доставлено').count()
+    pending = orders.filter(status='В ожидании').count()
+
+    context = {'orders': orders, 'customers': customers,
+               'total_orders': total_orders, 'delivered': delivered,
+               'pending': pending}
+
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all() # запросить из базы все продукты
+    return render(request, 'accounts/products.html', {'products': products})
+    # название ключа в словаре, может быть любым, мы по нему потом обращаемся в html доке
 
 
 def customer(request):
